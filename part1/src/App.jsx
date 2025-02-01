3ff98b5034232d78f038c576058c4c2cfa851e87
Qiuyi <qiuyif@bu.edu>
2025-02-01 13:10:02 -0500

    a

---
+++ part1/src/App.jsx
@@ -0,0 +1,58 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {'Fundamentals of React'} {10}
+      </p>
+      <p>
+        {'Using props to pass data'} {7}
+      </p>
+      <p>
+        {'State of a component'} {14}
+      </p>
+      <p>Number of exercises {10+7+14}</p>
+    </div>
+  )
+}
+
+export default App
